338759f9ce22ea4a8a2c23937f3dab6846a63de1
Lovlesh Dagar <lovlesh.dagar.buildint.gmail.com>
2023-10-09 12:25:44 +0530

    updated

--- app.js
+++ app.js
@@ -417,27 +417,14 @@ function generatePDF(data) {
     doc.fontSize(18).text('Data from Database', { align: 'center' });
 
     data.forEach((row) => {
-      // Check if `row.before_inst_images` is a valid image path or binary data
-      if (typeof row.before_inst_images === 'string') {
-        // It's a file path, so embed the image using the path
         doc.image(row.before_inst_images, {
           fit: [250, 300],
           align: 'center',
           valign: 'center'
         });
-      } else if (Buffer.isBuffer(row.before_inst_images)) {
-        // It's binary data, so embed the image using the binary data
-        doc.image(row.before_inst_images, {
-          fit: [250, 300],
-          align: 'center',
-          valign: 'center'
-        });
-      }
-      
       // Add a separator between entries
       doc.fontSize(12).text('---');
     });
-
     doc.end(); // Finish the PDF document
   });
 }
