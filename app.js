4f60270971f6b4c79b2765072163d437baf60487
LovleshDaagr <lovlesh.dagar@buildint.co>
2023-11-02 18:41:23 +0530

    updated

--- app.js
+++ app.js
@@ -240,7 +240,7 @@ function authenticateToken(req, res, next) {
 // Protected route that requires authentication
 app.get('/tokenVerify', authenticateToken, (req, res) => {
   // You can access user information from `req.user`
-  res.json({ message: 'Success', 'Role': req.user });
+  res.json({ message: 'Success', 'user_details': req.user });
 });
 
 // routes
